7dd4df212166ad557bb455c3dfbf0f48103ca289
esther0717git <liangwy@sea.com>
2025-09-19 11:46:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ st.markdown(
 # ───── Download Sample Template ───────────────────────────────────────────────
 with open("US_Template.xlsx", "rb") as f:
     st.download_button(
-        label="📎 Download US Template",
+        label="⬇️ Download US Template",
         data=f,
         file_name="US_Template.xlsx",
         mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
